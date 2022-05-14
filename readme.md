695943a792ad24c3a5077f049d15f717f29e6815
Viktor Sivulskiy <info@softmaker.kz>
2022-05-14 08:35:17 +0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Leocode interview tech task
+
+## Setup
+
+Run `yarn install` to install the packages.
+Run `yarn dev` to start the app.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import {AutosuggestionSelect} from './components/AutosuggestionSelect'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <AutosuggestionSelect />
+    </main>
+  )
+}
